272ceb435c7167e73a46272671b8e4ed7900b177
sivahariganesan.s <sivahariganesan.s@zohocorp.com>
2022-08-05 20:00:36 +0530

    Tauto and Alchemy APIs added

---
+++ tautotree-server/src/api/tatum-config/routes/tatum-config.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * tatum-config router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::tatum-config.tatum-config');
